5d4f1cb89dd73b03d48c0993e233c10685bc7cb9
Víctor Chagas <victorchagaslm@gmail.com>
2025-09-30 18:31:54 -0300

    fix: fixing script

--- scripts/configure_events.py
+++ scripts/configure_events.py
@@ -1,17 +1,17 @@
 from minio import Minio
-from minio.notificationconfig import NotificationConfig, QueueConfig
+from minio.notificationconfig import NotificationConfig, WebhookConfig
 
 client = Minio(
-    "minio-service:9000",
+    "127.0.0.1:9000",
     access_key="farmonedge",
     secret_key="farmonedge",
     secure=False
 )
 
 config = NotificationConfig(
-    queue_config_list=[
-        QueueConfig(
-            "arn:minio:sqs::1:webhook",
+    webhook_config_list=[
+        WebhookConfig(
+            "arn:minio:sqs:us-east-1:1:webhook",
             ["s3:ObjectCreated:*"],
         ),
     ],
@@ -20,7 +20,7 @@ config = NotificationConfig(
 try:
     client.set_bucket_notification("insect-images", config)
 
-    print("✅ Regra de notificação configurada com sucesso para o bucket 'insect-images'!")
+    print("✅ Regra de notificação por Webhook configurada com sucesso para o bucket 'insect-images'!")
     print("O watcher agora receberá os eventos do MinIO.")
 except Exception as e:
     print(f"❌ Ocorreu um erro ao configurar a notificação: {e}")
